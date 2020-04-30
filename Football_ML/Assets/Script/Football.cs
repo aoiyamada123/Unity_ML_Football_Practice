583ef9896728e5642b3f2f57a61580f386a642d9
keith <s101315@gmail.com>
2020-05-01 02:10:12 +0800

    Update

--- Football_ML/Assets/Script/Football.cs
+++ Football_ML/Assets/Script/Football.cs
@@ -6,8 +6,6 @@ public class Football : MonoBehaviour
 {
     public static bool goal;
 
-    public static float currentPositionZ;
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("GoalNet"))
